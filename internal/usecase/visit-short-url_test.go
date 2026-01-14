2d3b038ce417bbc6d0a9013a37fed09c5cb1b158
Exanubes <blog@exanubes.com>
2026-01-14 09:54:12 +0100

    Fix test

--- internal/usecase/visit-short-url_test.go
+++ internal/usecase/visit-short-url_test.go
@@ -1,27 +1,23 @@
 package usecase
 
-//
-// import (
-// 	"context"
-// 	"testing"
-//
-// 	"github.com/exanubes/url-shortener/internal/app/encoder"
-// 	"github.com/exanubes/url-shortener/internal/domain"
-// 	"github.com/exanubes/url-shortener/internal/infrastructure/persistence/inmemory"
-// )
-//
-// func TestVisitShortUrl(t *testing.T) {
-// 	provider := inmemory.NewInmemoryRepository()
-// 	codec := encoder.New()
-// 	long_url := "https://exanubes.com"
-// 	short_url := "2TX"
-// 	id := uint64(11_157)
-// 	provider.Save(context.TODO(), domain.Url{ID: id, Short: short_url, Long: long_url})
-//
-// 	usecase := NewVisitShortUrl(provider, codec)
-// 	result, _ := usecase.Execute(short_url)
-//
-// 	if result != long_url {
-// 		t.Fatalf("Expected: %s, received: %s", long_url, result)
-// 	}
-// }
+import (
+	"context"
+	"testing"
+
+	"github.com/exanubes/url-shortener/internal/domain"
+	"github.com/exanubes/url-shortener/internal/infrastructure/persistence/inmemory"
+)
+
+func TestVisitShortUrl(t *testing.T) {
+	provider := inmemory.NewInmemoryRepository()
+	long_url, _ := domain.NewUrl("https://exanubes.com")
+	short_code, _ := domain.NewShortCode("2TX", 7, "0")
+	provider.Save(context.TODO(), long_url, short_code)
+
+	usecase := NewVisitShortUrl(provider)
+	result, _ := usecase.Execute(context.TODO(), short_code.String())
+
+	if result.String() != long_url.String() {
+		t.Fatalf("Expected: %s, received: %s", long_url.String(), result.String())
+	}
+}
