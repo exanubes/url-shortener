aab726d017b5b6e4d1d6e9b11adc895492049efb
Exanubes <blog@exanubes.com>
2026-02-02 17:20:09 +0100

    Add build script

---
+++ scripts/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -e
+
+for lambda in create resolve visit; do
+  mkdir -p dist/$lambda
+  GOOS=linux GOARCH=arm64 go build -o dist/$lambda/bootstrap cmd/lambda/$lambda/main.go
+  (cd dist/$lambda && zip -q function.zip bootstrap)
+  echo "Built $lambda"
+done
