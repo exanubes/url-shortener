6f5266ca461282a747a4c91c59cc43668184a279
Exanubes <blog@exanubes.com>
2026-02-25 10:56:13 +0100

    Use snowflake id generator and feistel scrambler

--- cmd/lambda/create/main.go
+++ cmd/lambda/create/main.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"hash/fnv"
 	"log"
+	"os"
+	"time"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -10,9 +13,13 @@ import (
 	"github.com/exanubes/url-shortener/internal/app/services/shortcode"
 	createshorturl "github.com/exanubes/url-shortener/internal/app/usecases/create_short_url"
 	"github.com/exanubes/url-shortener/internal/infrastructure/api/lambda/create"
+	"github.com/exanubes/url-shortener/internal/infrastructure/clock"
 	encoding "github.com/exanubes/url-shortener/internal/infrastructure/encoding/base_62"
 	"github.com/exanubes/url-shortener/internal/infrastructure/persistence/dynamodb"
 )
+
+const feistel_key uint64 = 0x8c3f19d2e4a761b5
+const epoch = "2026-02-21T11:04:57.497"
 
 func main() {
 	ctx := context.Background()
@@ -21,11 +28,22 @@ func main() {
 		log.Fatal(err)
 	}
 	table := dynamodb.NewRepository(client)
-	token_generator := shortcode.NewGenerator(int64(7))
+	hash := fnv.New64a()
+	log_stream_name := os.Getenv("AWS_LAMBDA_LOG_STREAM_NAME")
+	hash.Write([]byte(log_stream_name))
+
+	epoch_date, err := time.Parse("2006-01-02T15:04:05.000", epoch)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	clock := clock.NewClock()
+	token_generator := shortcode.NewSnowflakeGenerator(hash, epoch_date, clock)
 	policy_factory := createshorturl.NewRetryPolicyFactory(3)
 	expiration_factory := expiration.NewFactory()
 	encoder := encoding.New()
-	shortcodes_service := shortcode.NewService(token_generator, encoder)
+	scrambler := shortcode.NewFeistel(feistel_key)
+	shortcodes_service := shortcode.NewService(token_generator, scrambler, encoder)
 	create_short_url_use_case := createshorturl.New(table, shortcodes_service, policy_factory, expiration_factory)
 	handler := create.NewHandler(create_short_url_use_case)
 	lambda.StartWithOptions(func(ctx context.Context, req events.APIGatewayV2HTTPRequest) (events.APIGatewayV2HTTPResponse, error) {
--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"context"
+	"hash/fnv"
 	"log"
+	"time"
 
 	"github.com/exanubes/url-shortener/internal/app/services/expiration"
 	"github.com/exanubes/url-shortener/internal/app/services/shortcode"
@@ -10,10 +12,14 @@ import (
 	resolveurl "github.com/exanubes/url-shortener/internal/app/usecases/resolve_url"
 	"github.com/exanubes/url-shortener/internal/domain"
 	"github.com/exanubes/url-shortener/internal/infrastructure/api/http"
+	"github.com/exanubes/url-shortener/internal/infrastructure/clock"
 	encoding "github.com/exanubes/url-shortener/internal/infrastructure/encoding/base_62"
 	"github.com/exanubes/url-shortener/internal/infrastructure/event"
 	"github.com/exanubes/url-shortener/internal/infrastructure/persistence/dynamodb"
 )
+
+const feistel_key uint64 = 0x8c3f19d2e4a761b5
+const epoch = "2026-02-21T11:04:57.497"
 
 func main() {
 	ctx := context.Background()
@@ -24,10 +30,16 @@ func main() {
 	table := dynamodb.NewRepository(client)
 	event_bus := event.NewBus(func(event domain.Event) error { return table.Visit(ctx, event.(domain.LinkVisited)) })
 	encoder := encoding.New()
-	token_generator := shortcode.NewGenerator(int64(7))
+	clock := clock.NewClock()
+	hash := fnv.New64a()
+	hash.Write([]byte("local_http_server"))
+
+	epoch_date, _ := time.Parse("2006-01-02T15:04:05.000", epoch)
+	token_generator := shortcode.NewSnowflakeGenerator(hash, epoch_date, clock)
+	scrambler := shortcode.NewFeistel(feistel_key)
 	policy_factory := createshorturl.NewRetryPolicyFactory(3)
 	expiration_factory := expiration.NewFactory()
-	shortcodes_service := shortcode.NewService(token_generator, encoder)
+	shortcodes_service := shortcode.NewService(token_generator, scrambler, encoder)
 	create_short_url_use_case := createshorturl.New(table, shortcodes_service, policy_factory, expiration_factory)
 	visit_url_use_case := resolveurl.New(table, table, event_bus)
 
